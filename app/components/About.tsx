acf2cc22f1a9f1ccda11793b7b4a79fea1f0408e
EJar74 <Eli.Jaramillo74@gmail.com>
2024-03-25 00:33:52 -0700

    more professional

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -17,12 +17,6 @@ const AboutSection = () => {
       <div className="container mx-auto flex flex-col items-center text-center">
         <h2 className="text-4xl md:text-5xl font-bold mb-6">About</h2>
         <div className="flex flex-col md:flex-row md:items-center space-y-6 md:space-y-0 md:space-x-6 bg-black rounded-lg p-4 md:p-8">
-          <div className="w-full md:w-1/3 mx-auto">
-            {/* Replace with your image path */}
-            <div className="inline-block overflow-hidden relative rounded-3xl w-32 h-48 md:w-48 md:h-72">
-              <Image src="/portrait.png" alt="Eli Jaramillo" layout="fill" objectFit="cover" />
-            </div>
-          </div>
           <div className="w-full text-gray-300 text-xs md:text-base">
             <p>
             {"I'm Eli Jaramillo, a passionate Software Engineer with a keen interest in stepping into the Product Management realm. My educational journey at the University of California, Irvine, where I earned dual degrees in Business Economics and Informatics with a specialization in Human-Computer Interaction, has equipped me with a unique blend of skills that bridge the gap between technology and business. My experience at Capital Group, managing projects that significantly improved operational efficiency and user satisfaction, has honed my ability to translate complex technical challenges into user-centric solutions. I'm driven by the challenge of innovating and optimizing products to enhance people's lives and businesses. Welcome to my portfolio, where I share my journey, projects, and the impact of my work."}
