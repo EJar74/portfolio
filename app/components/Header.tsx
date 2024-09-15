bb2abf4d2a44712b991b7da3d3086e7f13e72b9b
EliJ <Eli.Jaramillo74@gmail.com>
2024-09-14 18:20:19 -0700

    deleted old resumes, added new 2 column resume

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -99,7 +99,7 @@ const Header = () => {
                     </a>
                   ) : (
                     <a
-                      href="/Resume Eli Jaramillo PM.pdf"
+                      href="/Resume Eli Jaramillo Sep24.pdf"
                       target="_blank"
                       rel="noopener noreferrer"
                       className="text-lg font-medium cursor-pointer block py-2 md:py-0 px-4"
